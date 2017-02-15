c5f092a0834b724580825ee1ca0406ac3df2bd67
Navin Pathak <np4navin@gmail.com>
2017-02-14 22:25:10 -0600

    Added Initial server files

---
+++ app/routes.js
@@ -0,0 +1,16 @@
+module.exports = function(app) {
+
+	var path = require('path');
+
+	// server routes ===========================================================
+	// handle things like api calls
+	// authentication routes
+
+
+	// frontend routes =========================================================
+	// route to handle all angular requests
+	app.get('*', function(req, res, next) {
+		res.sendFile(path.join(__dirname, '../public', '/index.html'));
+	});
+
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+// modules =================================================
+var express = require('express');
+var pg = require('pg');
+
+var app = express();
+
+// configuration ===========================================
+
+var port = process.env.PORT || 4000; // set our port
+
+app.use(express.static(__dirname + '/public')); // set the static files location, example: /public/img will be /img for users
+
+// routes ==================================================
+require('./app/routes')(app); // pass our application into our routes
+
+// start app ===============================================
+app.listen(port);
+console.log('Listening on port ' + port); 			// shoutout to the user
+console.log('104.236.248.77:' + port);
+exports = module.exports = app; 						// expose app
