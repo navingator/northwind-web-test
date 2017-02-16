7a44c3b0df49c25ff38df37c674851fa2a4117fc
Navin Pathak <np4navin@gmail.com>
2017-02-15 18:16:06 -0600

    Added styling and bcryptjs

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  template: '<h1>Northwind App!!!</h1>',
+  templateUrl: './app.component.html',
   selector: 'northwind-app',
 })
 export class AppComponent { }
