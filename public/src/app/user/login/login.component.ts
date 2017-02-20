62f10bfe083382d797f03230be58a608cdf9d124
Navin Pathak <np4navin@gmail.com>
2017-02-20 16:41:16 -0600

    Cleaned up Sign in html/css

--- public/src/app/user/login/login.component.ts
+++ public/src/app/user/login/login.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+
+class User {
+  username: string;
+  password: string;
+}
 
 @Component({
   moduleId: module.id,
   templateUrl: './login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent {
+  user = new User();
+}
--- public/src/app/user/user.module.ts
+++ public/src/app/user/user.module.ts
@@ -1,6 +1,7 @@
 /** Angular Modules **/
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 /** Routing Module **/
 import { UserRoutingModule } from './user-routing.module';
@@ -12,6 +13,7 @@ import { SignupComponent } from './signup/signup.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     UserRoutingModule
   ],
   declarations: [
