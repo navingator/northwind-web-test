a082191741f246eaf15e778884161117fb3a40fb
Navin Pathak <np4navin@gmail.com>
2017-02-20 17:32:54 -0600

    Cleaned up login html/css

--- public/src/app/user/signup/signup.component.ts
+++ public/src/app/user/signup/signup.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 
+class NewUser {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  passwordConfirm: string;
+}
+
 @Component({
   moduleId: module.id,
   templateUrl: './signup.component.html',
 })
-export class SignupComponent { }
+export class SignupComponent {
+  newUser = new NewUser();
+}
