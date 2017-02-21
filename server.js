2c4e7c95e10ea2f012976cc0d7c5dda60d13e8be
Navin Pathak <np4navin@gmail.com>
2017-02-21 14:03:26 -0600

    Added browser-sync and easier way to set ports

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // modules =================================================
 var express = require('express');
+var browserSync = require('browser-sync')
 var path = require('path');
 var pg = require('pg');
+
+var ports = require('./ports.json');
 
 var app = express();
 
 // configuration ===========================================
-
-var port = process.env.PORT || 4000; // set our port
+var port = process.env.PORT || ports.server; // set our port
 
 app.use(express.static(path.join(__dirname, '/public'))); // set the static files location, example: /public/img will be /img for users
 app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
@@ -17,7 +19,19 @@ app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
-app.listen(port);
+app.listen(port, browserSyncListen);
 console.log('Listening on port ' + port); 			// shoutout to the user
-console.log('104.236.248.77:' + port);
+
+function browserSyncListen() {
+  browserSync({
+    proxy: 'localhost:' + port,
+    port: ports.browserSync,
+    files: ['public/**/*.{js,css,html}'],
+    ui: {
+      port: ports.browserSyncUI
+    },
+    open: false
+  });
+}
+
 exports = module.exports = app; 						// expose app
