0703376a245f3ab383927442168b5793c33554fc
Navin Pathak <np4navin@gmail.com>
2017-02-28 16:51:11 -0600

    Added requirement that username be unique

--- db/extensions/users.sql
+++ db/extensions/users.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
-  username character varying(15) NOT NULL,
+  username character varying(15) NOT NULL UNIQUE,
   first_name text NOT NULL,
   last_name text NOT NULL,
   password text NOT NULL,
