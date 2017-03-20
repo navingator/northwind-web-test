f90024b124ee412d74aecc667cb75f2a120d25a7
Navin Pathak <np4navin@gmail.com>
2017-03-20 09:40:33 -0500

    Added index to product categoryID

--- db/extensions/products.sql
+++ db/extensions/products.sql
@@ -2,14 +2,21 @@
 -- Create a sequence to generate product IDs
 CREATE SEQUENCE products_id_seq START WITH 100;
 
+-- Set Column properties
 ALTER TABLE products ALTER COLUMN productid SET DEFAULT nextval('products_id_seq'::regclass);
-ALTER TABLE products ADD UNIQUE (productname);
 ALTER TABLE products ALTER COLUMN categoryid SET NOT NULL;
 ALTER TABLE products ALTER COLUMN discontinued TYPE bool USING CASE WHEN discontinued=0 THEN FALSE ELSE TRUE END;
 ALTER TABLE products ALTER COLUMN discontinued SET DEFAULT 'f';
 ALTER TABLE products ALTER COLUMN productname TYPE text;
+
+-- Constraints
+ALTER TABLE products ADD UNIQUE (productname);
 ALTER TABLE products ADD CONSTRAINT products_productname_check CHECK (length(productname) >= 3 AND length(productname) <= 40);
 
+-- Indexes
+CREATE INDEX products_catid_index ON products(categoryid);
+
+-- Drop unused columns
 ALTER TABLE products DROP COLUMN supplierid;
 ALTER TABLE products DROP COLUMN quantityperunit;
 ALTER TABLE products DROP COLUMN unitsonorder;
