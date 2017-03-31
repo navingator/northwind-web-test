0356c6e6036b8ecd0dba3184a73e926c33887466
mknoedel <mknoedel@gmail.com>
2017-03-31 10:51:25 -0500

    fixing last commit.

--- public/src/app/product_category/search/category-search.component.ts
+++ public/src/app/product_category/search/category-search.component.ts
@@ -1,58 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { Router }            from '@angular/router';
-
-import { Observable }        from 'rxjs/Observable';
-import { Subject }           from 'rxjs/Subject';
-
-// Observable class extensions
-import 'rxjs/add/observable/of';
-
-// Observable operators
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-
-import { CategorySearchService } from '../category-search.service';
-
-import { ProdCat } from '../prodcat.class';
-
-@Component({
-  selector: 'category-name-search',
-  templateUrl: './hero-search.component.html',
-  styleUrls: [ './hero-search.component.css' ],
-  providers: [HeroSearchService]
-})
-export class HeroSearchComponent implements OnInit {
-  heroes: Observable<Hero[]>;
-  private searchTerms = new Subject<string>();
-
-  constructor(
-    private heroSearchService: HeroSearchService,
-    private router: Router) {}
-
-  // Push a search term into the observable stream.
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
-
-  ngOnInit(): void {
-    this.heroes = this.searchTerms
-      .debounceTime(300)        // wait 300ms after each keystroke before considering the term
-      .distinctUntilChanged()   // ignore if next search term is same as previous
-      .switchMap(term => term   // switch to new observable each time the term changes
-        // return the http search observable
-        ? this.heroSearchService.search(term)
-        // or the observable of empty heroes if there was no search term
-        : Observable.of<Hero[]>([]))
-      .catch(error => {
-        // TODO: add real error handling
-        console.log(error);
-        return Observable.of<Hero[]>([]);
-      });
-  }
-
-  gotoDetail(hero: Hero): void {
-    let link = ['/detail', hero.id];
-    this.router.navigate(link);
-  }
-}
+// import { Component, OnInit } from '@angular/core';
+// import { Router }            from '@angular/router';
+//
+// import { Observable }        from 'rxjs/Observable';
+// import { Subject }           from 'rxjs/Subject';
+//
+// // Observable class extensions
+// import 'rxjs/add/observable/of';
+//
+// // Observable operators
+// import 'rxjs/add/operator/catch';
+// import 'rxjs/add/operator/debounceTime';
+// import 'rxjs/add/operator/distinctUntilChanged';
+//
+// import { ProdCatSearchService } from '../prodcat-search.service';
+//
+// import { ProdCat } from '../prodcat.class';
+//
+// @Component({
+//   selector: 'category-name-search',
+//   templateUrl: './hero-search.component.html',
+//   styleUrls: [ './hero-search.component.css' ],
+//   providers: [ProdCatSearchService]
+// })
+// export class HeroSearchComponent implements OnInit {
+//   heroes: Observable<Hero[]>;
+//   private searchTerms = new Subject<string>();
+//
+//   constructor(
+//     private prodCatSearchService: ProdCatSearchService,
+//     private router: Router) {}
+//
+//   // Push a search term into the observable stream.
+//   search(term: string): void {
+//     this.searchTerms.next(term);
+//   }
+//
+//   ngOnInit(): void {
+//     this.prodCats = this.searchTerms
+//       .debounceTime(300)        // wait 300ms after each keystroke before considering the term
+//       .distinctUntilChanged()   // ignore if next search term is same as previous
+//       .switchMap(term: string => term   // switch to new observable each time the term changes
+//         // return the http search observable
+//         ? this.prodCatSearchService.search(term)
+//         // or the observable of empty heroes if there was no search term
+//         : Observable.of<Hero[]>([]))
+//       .catch(error => {
+//         // TODO: add real error handling
+//         console.log(error);
+//         return Observable.of<Hero[]>([]);
+//       });
+//   }
+//
+//   gotoDetail(hero: Hero): void {
+//     let link = ['/detail', hero.id];
+//     this.router.navigate(link);
+//   }
+// }
