de58ce4c081cca2d016a4818729ffe5addb19b87
Navin Pathak <np4navin@gmail.com>
2017-04-06 19:53:28 -0500

    Added server routes for release environment

--- app/controllers/core.controller.js
+++ app/controllers/core.controller.js
@@ -3,5 +3,10 @@
 var path = require('path');
 
 exports.index = function (req, res) {
-  res.sendFile(path.resolve('./public/src/index.html'));
+  console.log('node_env', process.env.NODE_ENV);
+  if(process.env.NODE_ENV === 'release' || process.env.NODE_ENV === 'production') {
+    res.sendFile(path.resolve('./public/dist/index.html'));
+  } else {
+    res.sendFile(path.resolve('./public/src/index.html'));
+  }
 };
